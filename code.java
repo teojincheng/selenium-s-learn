2eb9c0f0c4a181c26fbc7f1be0297477c903af97
Teo Jin Cheng <jinchengproject@gmail.com>
2020-04-29 11:04:15 +0800

    abstract test into seperate function and run function inside main()

--- code.java
+++ code.java
@@ -7,8 +7,62 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 public class MyClass {
+	
+	
+	public static boolean canAddItemToCart(WebDriver webDriver,WebDriverWait webDriverWait){
+		// declaration and instantiation of objects/variables
+    	//System.setProperty("webdriver.gecko.driver","/Users/User/Downloads/geckodriver-v0.24.0-win64/geckodriver.exe");
+		//WebDriver driver = new FirefoxDriver();
+		//WebDriverWait wait=new WebDriverWait(driver, 20);
+		//comment the above 2 lines and uncomment below 2 lines to use Chrome
+		//System.setProperty("webdriver.chrome.driver","G:\\chromedriver.exe");
+		//WebDriver driver = new ChromeDriver();
+    	
+        String baseUrl = "http://mtoys-167102.appspot.com/"; 
+        String cartNum = "";
+
+        // launch Fire fox and direct it to the Base URL
+        webDriver.get(baseUrl);
+
+        webDriver.findElement(By.linkText("Login")).click();
+        webDriver.findElement(By.cssSelector("input[type='submit']")).click();
+       WebElement toyElement = webDriver.findElement(By.xpath("//a[@href='itemDetail.php?id=19']"));
+       JavascriptExecutor jsExecutor = (JavascriptExecutor) webDriver;  
+       jsExecutor.executeScript("arguments[0].click();", toyElement);
+       
+       WebElement addCartElement;
+       addCartElement = webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div/div/div/div[1]/div[2]/form/input[4]")));
+       addCartElement.click();
+       //jsExecutor.executeScript("arguments[0].click();", addElement);
+        WebElement cartElement = webDriver.findElement(By.id("cartNum"));
+      
+        cartNum = cartElement.getText();
+        
+        //close Fire fox
+        webDriver.close();
+        
+        if(cartNum.contentEquals("1")){
+        	return true;
+        }
+        return false;
+       
+	}
+	
+	public static boolean itemCanBeDeleted(){
+		return false;
+	}
 
 	 public static void main(String[] args) {
+		 System.setProperty("webdriver.gecko.driver","/Users/User/Downloads/geckodriver-v0.24.0-win64/geckodriver.exe");
+		 WebDriver driver = new FirefoxDriver();
+			WebDriverWait wait=new WebDriverWait(driver, 20);
+		 
+		boolean itemIsAddedToCart = canAddItemToCart(driver,wait);
+		
+		if(itemIsAddedToCart){
+			System.out.println("Item can be added to cart");
+		}
+		 /*
 	        // declaration and instantiation of objects/variables
 	    	System.setProperty("webdriver.gecko.driver","/Users/User/Downloads/geckodriver-v0.24.0-win64/geckodriver.exe");
 			WebDriver driver = new FirefoxDriver();
@@ -17,18 +71,12 @@ public class MyClass {
 			//System.setProperty("webdriver.chrome.driver","G:\\chromedriver.exe");
 			//WebDriver driver = new ChromeDriver();
 	    	
-	        //String baseUrl = "http://demo.guru99.com/test/newtours/";
 	        String baseUrl = "http://mtoys-167102.appspot.com/"; 
-			String expectedTitle = "Welcome: Mercury Tours";
-	        String actualTitle = "";
 	        String cartNum = "";
 
 	        // launch Fire fox and direct it to the Base URL
 	        driver.get(baseUrl);
 
-	        // get the actual value of the title
-	        //actualTitle = driver.getTitle();
-	        //click on the login link
 	        driver.findElement(By.linkText("Login")).click();
 	        driver.findElement(By.cssSelector("input[type='submit']")).click();
 	       WebElement toyElement = driver.findElement(By.xpath("//a[@href='itemDetail.php?id=19']"));
@@ -48,20 +96,8 @@ public class MyClass {
 	        }else{
 	        	System.out.println("test failed");
 	        }
-	        
-	        /*
-	         * compare the actual title of the page with the expected one and print
-	         * the result as "Passed" or "Failed"
-	         */
-	        /*
-	        if (actualTitle.contentEquals(expectedTitle)){
-	            System.out.println("Test Passed!");
-	        } else {
-	            System.out.println("Test Failed");
-	        }
-	       */
 	        //close Fire fox
 	        driver.close();
-	       
+	       */
 	    }
 }
